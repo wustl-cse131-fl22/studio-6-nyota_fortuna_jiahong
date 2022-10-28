9b317b507c2fa65fad7c0b53c180d1ae2017c5be
shangwenyota <shangwenyota@shangwes-mbp.dhcp.wustl.edu>
2022-10-27 20:06:24 -0500

    Studio6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -1,5 +1,7 @@
 package studio6;
 
+import java.awt.Color;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class RecursiveMethods {
@@ -12,9 +14,11 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			if (n == 0) {
+				return 0;
+			}
+			
+			return Math.pow(0.5, n) + geometricSum(n-1);
 		
 	}
 
@@ -27,9 +31,11 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
+			if (q == 0) {
+				return p;
+			}
+			
+			return gcd(q, p % q);
 		
 	}
 
@@ -42,10 +48,27 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
+		int [] array2 = new int [array.length];
+		
+		if (array.length == 0) {
+			return array2;
+		}
 		
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+			return helperMethod(array, array2, array.length-1, 0);
 		
+	}
+	
+	public static int[] helperMethod(int [] array, int[] array2, int n, int counter) {
+		
+		if (n == 0) {
+			array2[counter] = array[n]; //if the array length is 0 return an empty array2
+			return array2; //at this point array2 should be filled reversliy
+		}
+		
+
+		array2[counter] = array[n];
+		return helperMethod(array, array2, n - 1, counter + 1);
 	}
 
 	/**
@@ -59,6 +82,19 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
+		
+		if (radius <= radiusMinimumDrawingThreshold) {
+			StdDraw.circle(xCenter, yCenter, radius);
+		}
+		
+		StdDraw.circle(xCenter, yCenter, radius);
+		
+		
+		 circlesUponCircles(xCenter + radius , yCenter , radius/3.0 ,radiusMinimumDrawingThreshold);
+		 circlesUponCircles(xCenter - radius , yCenter  , radius/3.0 ,radiusMinimumDrawingThreshold);
+		 circlesUponCircles(xCenter ,yCenter+radius  , radius/3.0 ,radiusMinimumDrawingThreshold);
+		 circlesUponCircles(xCenter ,yCenter - radius, radius/3.0 ,radiusMinimumDrawingThreshold);
+		
 		
 		// FIXME
 	}
